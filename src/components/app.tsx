f73ddc1ce45507584d125ffe478bd41f03feeaa7
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2025-01-04 10:26:44 +0700

    setup project

---
+++ src/components/app.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { App, ZMPRouter, AnimationRoutes, SnackbarProvider } from "zmp-ui";
+import { RecoilRoot } from "recoil";
+import HomePage from "pages/homepage";
+
+const MyApp = () => {
+  return (
+    <RecoilRoot>
+      <App>
+        <SnackbarProvider>
+          <ZMPRouter>
+            <AnimationRoutes>
+              <Routes>
+                <Route path="/" element={<HomePage></HomePage>}></Route>
+              </Routes>
+            </AnimationRoutes>
+          </ZMPRouter>
+        </SnackbarProvider>
+      </App>
+    </RecoilRoot>
+  );
+};
+export default MyApp;
---
+++ src/pages/homepage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Page } from "zmp-ui";
+
+const HomePage: React.FunctionComponent = () => {
+
+  return (
+    <Page className="page">
+      hello world
+    </Page>
+  );
+};
+
+export default HomePage;
