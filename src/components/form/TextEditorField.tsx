7ecf770f774a0dd1c68f1fb5fc920c1142f869bb
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2025-05-11 17:00:06 +0700

    [ADD] image in editor

--- src/components/form/TextEditorField.tsx
+++ src/components/form/TextEditorField.tsx
@@ -26,25 +26,25 @@ const TextEditorField: React.FC<TextEditorFieldProps> = ({
     <div style={{ paddingBottom: '16px', position: 'relative' }}>
       <Label name={name} text={label} required={required} />
       <ReactQuill
-        // modules={{
-        //   toolbar: {
-        //     container: [
-        //       [{ header: "1" }, { header: "2" }, { font: [] }],
-        //       [{ size: [] }],
-        //       ["bold", "italic", "underline", "strike", "blockquote"],
-        //       [
-        //         { list: "ordered" },
-        //         { list: "bullet" },
-        //         { indent: "-1" },
-        //         { indent: "+1" },
-        //       ],
-        //       ["image", "video"],
-        //     ],
-        //   },
-        //   clipboard: {
-        //     matchVisual: false,
-        //   },
-        // }}
+        modules={{
+          toolbar: {
+            container: [
+              [{ header: "1" }, { header: "2" }, { font: [] }],
+              [{ size: [] }],
+              ["bold", "italic", "underline", "strike", "blockquote"],
+              [
+                { list: "ordered" },
+                { list: "bullet" },
+                { indent: "-1" },
+                { indent: "+1" },
+              ],
+              ["image"],
+            ],
+          },
+          clipboard: {
+            matchVisual: false,
+          },
+        }}
         value={field.value || ''}
         onChange={(value) => field.onChange(value)}
         placeholder={placeholder || "Nhập nội dung..."}
